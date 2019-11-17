e9749cf411afafbd39f06beb503f304510de7779
luciebaker <luciebdev@gmail.com>
2019-11-17 09:23:46 -0500

    testing variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Forky's Restaurant`,
+    description: `restaurant site made with gatsby.`,
+    author: `LucieB`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,27 +18,32 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
-        // Add any options here
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, '500'],
+          },
+          {
+            family: `Raleway`,
+            variants: [`400`, `500`]
+          },
+        ],
+      },
     },
-  },
-  {
-    resolve: `gatsby-plugin-prefetch-google-fonts`,
-    options: {
-      fonts: [
-        {
-          family: `Montserrat`,
-          variants: [`400`, '500'],
-        },
-        {
-          family: `Raleway`,
-          variants: [`400`, `500`]
-        },
-      ],
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        // Learn about environment variables: https://gatsby.app/env-vars
+        accessToken: process.env.ACCESS_TOKEN,
+      },
     },
-  },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -49,7 +57,9 @@ module.exports = {
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
   ],
 }
+  
+    
